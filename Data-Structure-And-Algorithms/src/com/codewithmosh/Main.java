1ca4117a07a8d709a278ad874b28c52e983bbc05
Nur Mohammad Faisal <noormoy@gmail.com>
2022-05-10 23:00:31 +0600

    Array

---
+++ Data-Structure-And-Algorithms/src/com/codewithmosh/Main.java
@@ -0,0 +1,14 @@
+package com.codewithmosh;
+
+public class Main {
+    public static void main(String[] args){
+        Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.removeAt(0);
+        numbers.indexOf(20);
+        numbers.print();
+    }
+}
