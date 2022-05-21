edefcb18022fc5713dd5f49f977d7b7f98a3c6f0
Nur Mohammad Faisal <noormoy@gmail.com>
2022-05-21 09:17:56 +0600

    Stack - String Reverser

--- Data-Structure-And-Algorithms/src/Stack/Main.java
+++ Data-Structure-And-Algorithms/src/Stack/Main.java
@@ -4,16 +4,7 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args){
-        Stack<Integer> stack = new Stack<>();
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
-        System.out.println(stack);
-        var top = stack.pop();
-        System.out.println(top);
-        System.out.println(stack);
-        var top2 = stack.peek();
-        System.out.println(top2);
-        System.out.println(stack);
+        var stringReverser = new StringReverser();
+        System.out.println(stringReverser.reverse("abcdefgh"));
     }
 }
