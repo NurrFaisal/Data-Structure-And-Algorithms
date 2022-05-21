a5ba24189e6516a9df1b4a539b56c671727b5440
Nur Mohammad Faisal <noormoy@gmail.com>
2022-05-21 10:29:59 +0600

    stack - Expression refactor

--- Data-Structure-And-Algorithms/src/Stack/Expression.java
+++ Data-Structure-And-Algorithms/src/Stack/Expression.java
@@ -1,28 +1,42 @@
 package Stack;
 
+import com.codewithmosh.Array;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class Expression {
+
+    private final List<Character> leftBrackets = Arrays.asList('(', '<', '[', '{');
+    private final List<Character> rightBrackets = Arrays.asList(')', '>', ']', '}');
+
     public boolean isBalanced(String input){
         Stack<Character> stack = new Stack<>();
         for (char ch : input.toCharArray()){
-            if (ch == '(' || ch == '<' || ch == '[' || ch   == '{'){
+            if (isLeftBracked(ch)){
                 stack.push(ch);
             }
-            if (ch == ')' || ch == '>' || ch == ']' || ch == '}'){
+            if (isRightBracked(ch)){
                 if (stack.empty()) return false;
+
                 var top = stack.pop();
-                if(
-                        (ch == ')' && top != '(' ) ||
-                        (ch == '>' && top != '<' ) ||
-                        (ch == ']' && top != '[' ) ||
-                        (ch == '}' && top != '{' )
-                ){
+                if(isBrackedMatch(top, ch)){
                     return false;
                 }
             }
         }
         return stack.empty();
+    }
+
+    private boolean isLeftBracked(char ch){
+        return leftBrackets.contains(ch);
+    }
+    private boolean isRightBracked(char ch){
+        return rightBrackets.contains(ch);
+    }
+    private boolean isBrackedMatch(char left, char right){
+        return leftBrackets.indexOf(left) == rightBrackets.indexOf(right);
     }
 
 }
