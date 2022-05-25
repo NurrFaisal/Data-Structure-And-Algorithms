96808628d3adf958c81f2013747422e1221a7fee
NurFaisal <nurfaisal@nexkraft.com>
2022-05-25 09:35:48 +0600

    Queue - Reverse

--- Data-Structure-And-Algorithms/src/Queue/Main.java
+++ Data-Structure-And-Algorithms/src/Queue/Main.java
@@ -10,9 +10,21 @@ public class Main {
         queue.add(10);
         queue.add(20);
         queue.add(30);
-        var front = queue.remove();
-        System.out.println(queue);
-        System.out.println(front);
+//        var front = queue.remove();
+//        System.out.println(queue);
+//        System.out.println(front);
+//        System.out.println(queue);
+        reverse(queue);
+    }
+
+    public static void reverse(Queue<Integer> queue){
+        Stack<Integer> stack = new Stack<>();
+        while (!queue.isEmpty()){
+            stack.push(queue.remove());
+        }
+        while (!stack.isEmpty()){
+            queue.add(stack.pop());
+        }
         System.out.println(queue);
     }
 
