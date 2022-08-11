495e2f2e456e2e95880e1f35833980a65ba1eec7
Nur Mohammad Faisal <noormoy@gmail.com>
2022-08-11 06:18:12 +0600

    traverse with pre-order, in-order and post order

--- Data-Structure-And-Algorithms/src/BinaryTree/Main.java
+++ Data-Structure-And-Algorithms/src/BinaryTree/Main.java
@@ -9,6 +9,7 @@ public class Main {
         tree.insert(6);
         tree.insert(8);
         tree.insert(10);
-        System.out.println(tree.find(10));
+//        System.out.println(tree.find(10));
+        tree.traversePreOrder();
     }
 }
