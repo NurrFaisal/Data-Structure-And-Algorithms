263664d036bc3313e540e6f044c1694d870fb958
Nur Mohammad Faisal <noormoy@gmail.com>
2022-08-28 07:12:45 +0600

    heapify

---
+++ Data-Structure-And-Algorithms/src/Heap/Main.java
@@ -0,0 +1,20 @@
+package Heap;
+
+import Array.Array;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+//        var heap = new Heap();
+//        heap.insert(10);
+//        heap.insert(17);
+//        heap.insert(5);
+//        heap.insert(22);
+//        heap.insert(4);
+//        System.out.println("Done");
+        int[] numbers = {5, 3, 10, 1, 4, 2};
+        MaxHeap.heapify(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
