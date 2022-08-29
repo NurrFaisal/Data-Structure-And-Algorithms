3a7c4fa076920990f9df631dc49708880a74ef21
Nur Mohammad Faisal <noormoy@gmail.com>
2022-08-29 07:43:22 +0600

    Trie - insert

---
+++ Data-Structure-And-Algorithms/src/Trie/Trie.java
@@ -0,0 +1,33 @@
+package Trie;
+
+public class Trie {
+    private class Node{
+        public static int ALPHABET_SIZE = 26;
+        private char value;
+        private Node[] children = new Node[ALPHABET_SIZE];
+        private boolean isEndOfWord;
+
+        public Node(char value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "value=" + value;
+        }
+    }
+
+    private Node root = new Node(' ');
+
+    public void insert(String word){
+        var current = root;
+        for (var ch : word.toCharArray()){
+            var index = ch - 'a';
+            if(current.children[index] == null){
+                current.children[index] = new Node(ch);
+            }
+            current = current.children[index];
+        }
+        current.isEndOfWord = true;
+    }
+}
