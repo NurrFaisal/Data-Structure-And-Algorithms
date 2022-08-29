c5183caca80f8dc7bad5251e4e00edf0464b7c36
Nur Mohammad Faisal <noormoy@gmail.com>
2022-08-29 07:58:43 +0600

    Trie - Abstraction

--- Data-Structure-And-Algorithms/src/Trie/Trie.java
+++ Data-Structure-And-Algorithms/src/Trie/Trie.java
@@ -1,10 +1,12 @@
 package Trie;
 
+import java.util.HashMap;
+
 public class Trie {
     private class Node{
         public static int ALPHABET_SIZE = 26;
         private char value;
-        private Node[] children = new Node[ALPHABET_SIZE];
+        private HashMap<Character, Node> children = new HashMap<>();
         private boolean isEndOfWord;
 
         public Node(char value) {
@@ -15,18 +17,28 @@ public class Trie {
         public String toString() {
             return "value=" + value;
         }
+        public boolean hasChild(char ch){
+            return children.containsKey(ch);
+        }
+        public void addChild(char ch){
+            children.put(ch, new Node(ch));
+        }
+        public Node getChild(char ch){
+            return children.get(ch);
+        }
     }
 
     private Node root = new Node(' ');
 
+
+
     public void insert(String word){
         var current = root;
         for (var ch : word.toCharArray()){
-            var index = ch - 'a';
-            if(current.children[index] == null){
-                current.children[index] = new Node(ch);
+            if(!current.hasChild(ch)){
+                current.addChild(ch);
             }
-            current = current.children[index];
+            current = current.getChild(ch);
         }
         current.isEndOfWord = true;
     }
