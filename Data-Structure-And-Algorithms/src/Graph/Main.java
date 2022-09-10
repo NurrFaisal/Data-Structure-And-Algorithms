10920ac316a216719917cfed22611e14262b0bc3
Nur Mohammad Faisal <noormoy@gmail.com>
2022-09-10 12:32:05 +0600

    graph -> topologicalSort

--- Data-Structure-And-Algorithms/src/Graph/Main.java
+++ Data-Structure-And-Algorithms/src/Graph/Main.java
@@ -3,14 +3,18 @@ package Graph;
 public class Main {
     public static void main(String[] names){
         var graph = new Graph();
+        graph.addNode("X");
         graph.addNode("A");
         graph.addNode("B");
-        graph.addNode("C");
-        graph.addNode("D");
-        graph.addEdge("A", "B");
-        graph.addEdge("A", "C");
+        graph.addNode("P");
+        graph.addEdge("X", "A");
+        graph.addEdge("X", "B");
+        graph.addEdge("A", "P");
+        graph.addEdge("B", "P");
 //        graph.removeEdge("A", "C");
 //        graph.print();
-        graph.traversDepthFirst("A");
+//        graph.traversDepthFirst("A");
+        var list = graph.topologicalSort();
+        System.out.println(list);
     }
 }
