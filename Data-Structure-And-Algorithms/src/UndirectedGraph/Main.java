fcedeaaa8a0c9294513a33a1db66d2efdca8f3be
Nur Mohammad Faisal <noormoy@gmail.com>
2022-09-11 08:03:35 +0600

    undirected graph -> weighted graph

---
+++ Data-Structure-And-Algorithms/src/UndirectedGraph/Main.java
@@ -0,0 +1,13 @@
+package UndirectedGraph;
+
+public class Main {
+    public static void main(String[] names){
+        var graph = new WeightedGraph();
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addEdge("A", "B", 3);
+        graph.addEdge("A", "C", 2);
+        graph.print();
+    }
+}
