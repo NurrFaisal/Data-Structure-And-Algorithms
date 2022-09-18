59e1bbf299c41aba96d0bd65a55d64d523fc3f8e
Nur Mohammad Faisal <noormoy@gmail.com>
2022-09-18 08:25:52 +0600

    sort-> bubbleSort

---
+++ Data-Structure-And-Algorithms/src/Sort/BubbleSort.java
@@ -0,0 +1,23 @@
+package Sort;
+
+public class BubbleSort {
+    public void sort(int[] array){
+        boolean isSorted =true;
+        for(var i = 0; i < array.length; i++){
+            for (var j = 1; j < array.length - i; j++){
+                if(array[j] < array[j - 1]){
+                    swap(array, j, j - 1);
+                    isSorted = false;
+                }
+            }
+            if (isSorted){
+                return;
+            }
+        }
+    }
+    public void swap(int[] array, int index1, int index2){
+        var temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+}
