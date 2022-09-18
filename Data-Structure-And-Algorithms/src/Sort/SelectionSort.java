138d956f52fce7c1c62f4d5690461ed4dad19a81
Nur Mohammad Faisal <noormoy@gmail.com>
2022-09-18 08:48:05 +0600

    Sort -> Selection Sort

---
+++ Data-Structure-And-Algorithms/src/Sort/SelectionSort.java
@@ -0,0 +1,20 @@
+package Sort;
+
+public class SelectionSort {
+    public void sort(int[] array){
+        for(var i = 0; i < array.length; i++){
+            var minIndex = i;
+            for (var j = i; j < array.length; j++){
+                if(array[j] < array[minIndex]){
+                    minIndex = j;
+                }
+            }
+            swap(array, minIndex, i);
+        }
+    }
+    public void swap(int[] array, int index1, int index2){
+        var temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+}
