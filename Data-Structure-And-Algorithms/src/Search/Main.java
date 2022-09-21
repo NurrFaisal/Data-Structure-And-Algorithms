1b9b6c4aa96375c8403e578404ea16d7313cdd4a
Nur Mohammad Faisal <noormoy@gmail.com>
2022-09-21 06:24:53 +0600

    Search -> binarySearch

--- Data-Structure-And-Algorithms/src/Search/Main.java
+++ Data-Structure-And-Algorithms/src/Search/Main.java
@@ -5,7 +5,7 @@ public class Main {
 //        int[] numbers = {7, 1, 3, 6, 5};
         int[] numbers = {1, 3, 5, 6, 7};
         var search = new Search();
-        var index = search.binarySearchRec(numbers, 5);
+        var index = search.binarySearch(numbers, 5);
         System.out.println(index);
     }
 }
