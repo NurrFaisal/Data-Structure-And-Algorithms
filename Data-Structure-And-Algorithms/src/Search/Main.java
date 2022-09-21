29aea6a3dccd145eeb3e82b2f6d4eb7d32aa43ac
Nur Mohammad Faisal <noormoy@gmail.com>
2022-09-21 06:18:15 +0600

    Search -> binarySeachRec

--- Data-Structure-And-Algorithms/src/Search/Main.java
+++ Data-Structure-And-Algorithms/src/Search/Main.java
@@ -2,9 +2,10 @@ package Search;
 
 public class Main {
     public static void main(String[] names){
-        int[] numbers = {7, 1, 3, 6, 5};
+//        int[] numbers = {7, 1, 3, 6, 5};
+        int[] numbers = {1, 3, 5, 6, 7};
         var search = new Search();
-        var index = search.linearSearch(numbers, 3);
+        var index = search.binarySearchRec(numbers, 5);
         System.out.println(index);
     }
 }
